155fe1600ca443cca2570ce60b0b9a1f335b20a1
David Barratt <david.barratt@golfchannel.com>
2016-11-20 18:47:34 -0500

    Update tests

--- src/Plugin/Block/HelloWorld.php
+++ src/Plugin/Block/HelloWorld.php
@@ -3,6 +3,7 @@
 namespace Drupal\hello_world\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
@@ -59,21 +60,39 @@ class HelloWorld extends BlockBase implements ContainerFactoryPluginInterface {
    */
   public function build() {
     $storage = $this->entityTypeManager->getStorage('node');
+
+    // Get the Hello World Articles that are in enabled sections.
     $query = $storage->getQuery();
+    $query->condition('type', 'hello_world_article');
     $query->condition('field_sections.entity.field_enabled', 1);
+    $query->sort('created', 'DESC');
     $result = $query->execute();
     $entities = $storage->loadMultiple($result);
 
+    // Generate the initial cacheable metadata.
     $cache = new CacheableMetadata();
-    $cache->setCacheTags($storage->getEntityType()->getListCacheTags());
+
+    // Any node or taxonomy term that is updated should bust the cache.
+    // Ideally we would only purge when updates are made to the known bundles,
+    // but we'll have to wait until #2145751
+    // ({@link https://www.drupal.org/node/2145751}) is reolsved.
+    $node_tags = $storage->getEntityType()->getListCacheTags();
+    // Since a taxonomy term could be changed from 'enabled' to 'disabled',
+    // we'll need to bush the cache when any taxonomy term is updated.
+    $taxonomy_tags = $this->entityTypeManager->getStorage('taxonomy_term')->getEntityType()->getListCacheTags();
+    $cache->setCacheTags(Cache::mergeTags($node_tags, $taxonomy_tags));
 
     $build = [];
     if (!empty($entities)) {
       $links = [];
 
       foreach ($entities as $entity) {
+        // Individual entities could have their own cacheable metadata and
+        // should be merged with the the overall cache object.
         $cache = $cache->merge(CacheableMetadata::createFromObject($entity));
 
+        // Access results could have their own cacheable metadata and
+        // should be merged with the the overall cache object.
         $access = $entity->access('view', NULL, TRUE);
         $cache = $cache->merge(CacheableMetadata::createFromObject($access));
 
--- tests/src/Unit/Plugin/Block/HelloWorldTest.php
+++ tests/src/Unit/Plugin/Block/HelloWorldTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\hello_world\Unit\Plugin\Block;
 
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\hello_world\Plugin\Block\HelloWorld;
 use Drupal\Tests\UnitTestCase;
 
@@ -21,18 +23,99 @@ class HelloWorldTest extends UnitTestCase {
     $plugin_definition = [
       'provider' => 'hello_world',
     ];
-    $query = $this->getMock('Drupal\Core\Entity\Query\QueryInterface');
-    $entity_query = $this->getMockBuilder('Drupal\Core\Entity\Query\QueryFactory')
-      ->disableOriginalConstructor()
-      ->getMock();
-    $entity_query->method('get')
-      ->with($this->equalTo('node'))
-      ->willReturn($query);
 
-    $block = new HelloWorld($configuration, $plugin_id, $plugin_definition, $entity_query);
+    $query = $this->getMock('Drupal\Core\Entity\Query\QueryInterface');
+    $node_type = $this->getMock('Drupal\Core\Entity\EntityTypeInterface');
+    $node_type->expects($this->exactly(2))
+      ->method('getListCacheTags')
+      ->willReturn(['node_list']);
+    $taxonomy_term_type = $this->getMock('Drupal\Core\Entity\EntityTypeInterface');
+    $taxonomy_term_type->expects($this->exactly(2))
+      ->method('getListCacheTags')
+      ->willReturn(['taxonomy_term_list']);
+
+    $node_storage = $this->getMock('Drupal\Core\Entity\EntityStorageInterface');
+    $node_storage->expects($this->exactly(2))
+      ->method('getQuery')
+      ->willReturn($query);
+    $node_storage->expects($this->exactly(2))
+      ->method('getEntityType')
+      ->willReturn($node_type);
+
+    $taxonomy_term_storage = $this->getMock('Drupal\Core\Entity\EntityStorageInterface');
+    $taxonomy_term_storage->expects($this->exactly(2))
+      ->method('getEntityType')
+      ->willReturn($taxonomy_term_type);
+
+    $entity_type_manager = $this->getMock('Drupal\Core\Entity\EntityTypeManagerInterface');
+    $entity_type_manager->expects($this->exactly(4))
+      ->method('getStorage')
+      ->will($this->returnValueMap([
+        ['node', $node_storage],
+        ['taxonomy_term', $taxonomy_term_storage],
+      ]));
+
+    // Test an empty query result.
+    $block = new HelloWorld($configuration, $plugin_id, $plugin_definition, $entity_type_manager);
     $build = $block->build();
 
     $this->assertInternalType('array', $build);
+    $this->assertCount(1, $build);
+    $this->assertArrayHasKey('#cache', $build);
+    $this->assertArrayHasKey('tags', $build['#cache']);
+    $this->assertEquals(['node_list', 'taxonomy_term_list'], $build['#cache']['tags']);
+
+    // Test a simple simple result set.
+    $denied = $this->getMock('Drupal\Core\Access\AccessResultInterface');
+    $denied->expects($this->once())
+      ->method('isAllowed')
+      ->willReturn(FALSE);
+    $first = $this->getMock('Drupal\node\NodeInterface');
+    $first->expects($this->once())
+      ->method('access')
+      ->willReturn($denied);
+    $first->expects($this->once())
+      ->method('getCacheTags')
+      ->willReturn(['node:1']);
+
+    $link = new Link('Second', new Url('entity.node.canonical', ['node' => 2]));
+    $allowed = $this->getMock('Drupal\Core\Access\AccessResultInterface');
+    $allowed->expects($this->once())
+      ->method('isAllowed')
+      ->willReturn(TRUE);
+    $second = $this->getMock('Drupal\node\NodeInterface');
+    $second->expects($this->once())
+      ->method('access')
+      ->willReturn($allowed);
+    $second->expects($this->once())
+      ->method('getCacheTags')
+      ->willReturn(['node:2']);
+    $second->expects($this->once())
+      ->method('toLink')
+      ->willReturn($link);
+
+    $node_storage->expects($this->once())
+      ->method('loadMultiple')
+      ->willReturn([
+        $first,
+        $second,
+      ]);
+
+    $build = $block->build();
+
+    $this->assertInternalType('array', $build);
+    $this->assertCount(3, $build);
+    $this->assertArrayHasKey('#cache', $build);
+    $this->assertArrayHasKey('tags', $build['#cache']);
+    $this->assertEquals(['node:1', 'node:2', 'node_list', 'taxonomy_term_list'], $build['#cache']['tags']);
+    $this->assertArrayHasKey('#links', $build);
+    $this->assertCount(1, $build['#links']);
+    $this->assertArrayHasKey(0, $build['#links']);
+    $this->assertArrayHasKey('title', $build['#links'][0]);
+    $this->assertEquals('Second', $build['#links'][0]['title']);
+    $this->assertArrayHasKey('url', $build['#links'][0]);
+    $this->assertEquals('Second', $build['#links'][0]['title']);
+    $this->assertInstanceOf(Url::class, $build['#links'][0]['url']);
   }
 
 }
